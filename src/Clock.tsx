24744c02fe9accf5d22a76f21980783465147bac
Aaron Lin <aaronlin74@gmail.com>
2022-02-26 23:26:37 +1100

    Added clock functionality

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import './Clock.scss';
 
 const Clock = () => {
+  const hourRef = useRef<HTMLDivElement>(null);
+  const minuteRef = useRef<HTMLDivElement>(null);
+  const secondRef = useRef<HTMLDivElement>(null);
+
+  /**
+   * Set the ratio of rotation for second, minute, and hour hands.
+   * Added second ratio to the minute ratio for smooth minute update.
+   * Same for hour ratio.
+   * Set relative hand's element css variable '--rotation' property
+   */
+  const setClock = () => {
+    const currentDate: Date = new Date();
+    const secondRatio: number = currentDate.getSeconds() / 60;
+    const minuteRatio: number = (secondRatio + currentDate.getMinutes()) / 60;
+    const hourRatio: number = (minuteRatio + currentDate.getHours()) / 12;
+    secondRef.current?.style.setProperty(
+      '--rotation',
+      `${secondRatio * 360}deg`
+    );
+    minuteRef.current?.style.setProperty(
+      '--rotation',
+      `${minuteRatio * 360}deg`
+    );
+    hourRef.current?.style.setProperty('--rotation', `${hourRatio * 360}deg`);
+  };
+
+  // Set position of the clock hands during re-rendering
+  useLayoutEffect(() => {
+    setClock();
+    const clockInterval = setInterval(setClock, 1000);
+    return () => clearInterval(clockInterval);
+  }, []);
+
   return (
     <div className='clock'>
-      <div className='stand hour'></div>
-      <div className='stand minute'></div>
-      <div className='stand second'></div>
+      <div className='stand hour' ref={hourRef}></div>
+      <div className='stand minute' ref={minuteRef}></div>
+      <div className='stand second' ref={secondRef}></div>
+      {}
       <div className='number number1'>1</div>
       <div className='number number2'>2</div>
       <div className='number number3'>3</div>
